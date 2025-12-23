1e9eb2434bfce6124e3e2a64e249253e9cbd2bde
Joshua Edward McLaughlin Cox <josh@webhosting.coop>
2025-12-23 17:26:22 -0600

    benchmark_classic

--- scripts/benchmark-loop.sh
+++ scripts/benchmark-loop.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/bash
-EDIT_FORMATS=$(shuf benchmark/formats.list)
-MODELS=$(shuf models.list)
+: ${EDIT_FORMATS:=$(shuf benchmark/formats.list)}
+: ${MODELS:=$(shuf models.list)}
+: ${BENCH_CMD:='./benchmark/benchmark_classic.py'}
 if [[ -f .env ]]; then
   source .env
 fi
 for format  in ${EDIT_FORMATS}; do
   for model in ${MODELS}; do
-  ./benchmark/benchmark.py ${model}-${format} \
+  ${BENCH_CMD} \
+    ${model}-${format} \
     --new \
     --model "ollama_chat/${model}" \
     --edit-format ${format} \
